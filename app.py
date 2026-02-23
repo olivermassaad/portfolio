c3b6d1788c3d61e93832a27ffb06b206bb2688ff
Oliver <olimasad@gmail.com>
2026-02-22 19:16:50 -0500

    added to achievements

--- app.py
+++ app.py
@@ -12,7 +12,90 @@ def projects():
 
 @app.route('/achievements')
 def achievements():
-    return render_template('achievements.html')
+    achievements = [
+        {
+            "id": "ConUHacks X",
+            "victory": True,
+            "done": True,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "HackDécouverte",
+            "victory": True,
+            "done": True,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "Science On Tourne!",
+            "victory": False,
+            "done": True,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "Dialogue Employees Hackathon",
+            "victory": False,
+            "done": True,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "UdeM Hacks",
+            "victory": None,
+            "done": False,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "McGill Aerohacks",
+            "victory": None,
+            "done": False,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "VanierHacks!",
+            "victory": None,
+            "done": False,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "JacHacks",
+            "victory": None,
+            "done": False,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        },
+        {
+            "id": "MariHacks",
+            "victory": True,
+            "done": False,
+            "date": "1/01/2000",
+            "participants": 1,
+            "teammates": "Jongmin Lee, Evan Luo, Tariq Menla",
+            "description": "description here"
+        }
+    ]
+    return render_template('achievements.html', achievements=achievements)
 
 @app.route('/contact')
 def contact():
