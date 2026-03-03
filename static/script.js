85615cad283a5c36c252aa6dda85778f47375dea
Oliver <olimasad@gmail.com>
2026-03-03 00:58:13 -0500

    contact page

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
 // js
+
+"use strict"
+
+const githubButton = document.getElementById("githubbutton");
+githubButton.addEventListener("click", github);
+function github() {
+    window.location.href = 'https://github.com/olivermassaad';
+};
+
+const linkedInButton = document.getElementById("linkedinbutton");
+linkedInButton.addEventListener("click", linkedin);
+function linkedin() {
+    window.location.href = 'https://www.linkedin.com/in/oliver-massaad-9765a0276/?originalSubdomain=ca';
+};
